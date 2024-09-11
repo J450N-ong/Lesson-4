1e9fde8f9cb05ad3c9ec8fa87872df3a7500fba3
J450N-ong <odeleyejason@gmail.com>
2024-09-11 19:45:50 +0100

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,11 @@
+tree1=62
+tree2=97
+tree3=51
+tree4=89
+tree5=70
+
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of 5 numbers is " ,sum)
+
+average=sum/5
+print("The average of the sum of the 5 numbers are ",average)
